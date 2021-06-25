8a93445c33081509f13ef796226bbf024de1a3c6
fodro <feodor.tomilov@yandex.ru>
2021-06-25 17:56:20 +0300

    feat: implemented filling about

--- src/api/user.ts
+++ src/api/user.ts
@@ -9,12 +9,98 @@ export type LoginData = {
 	username: string,
 	password: string,
 }
+/*
+{
+  "identifier": 1,
+  "name": "string",
+  "surname": "string",
+  "city": "string",
+  "birthday": "2021-06-24T11:10:19.842Z",
+  "gender": "string",
+  "citizenships": [
+    "string"
+  ],
+  "rank": "string",
+  "salary": 0,
+  "currency": "string",
+  "stack": [
+    "string"
+  ],
+  "school": "string",
+  "age": 0,
+  "native_language": "string",
+  "foreign_languages": [
+    "string"
+  ],
+  "can_move": "string",
+  "metro_station": "string",
+  "github_id": "string",
+  "vk_id": "string",
+  "telegram_id": "string",
+  "timetable": [
+    {
+        "day": "day",
+        "time": "time"
+    }
+  ],
+  "achievements": [
+    {
+        "type": "Type",
+		"title": "Title",
+		"level": "Level",
+		"role": "Role",
+		"file": "File",
+		"description": "Description"
+    }
+  ]
+}
+
+*/
+
+export type Timetable = {
+	day: string,
+	time: string,
+}
+
+export type Achievement = {
+	type: string,
+	title: string,
+	level: string,
+	role: string,
+	// file: string,
+	description: string,
+}
+
+export type FillAboutData = {
+	identifier: string | number,
+	name: string,
+	surname: string,
+	city: string,
+	birthday: string,
+	gender: string,
+	citizenships: string[],
+	rank: string,
+	salary: number,
+	currency: string,
+	stack: string[],
+	school: string,
+	age: number,
+	native_language: string,
+	foreign_languages: string[],
+	can_move: string,
+	metro_station: string,
+	github_id: string,
+	vk_id: string,
+	telegram_id: string,
+	timetable: Timetable[],
+	achievements: Achievement[],
+}
 
 let host = process.env["WANTIT_BACK"]
 if (!host)
 	host = "localhost/api"
 
-export const NewUser = async (data: NewUserData) => {
+export const newUser = async (data: NewUserData) => {
 	const response = await fetch(
 		host + "/auth/registration",
 		{
@@ -33,9 +119,28 @@ export const NewUser = async (data: NewUserData) => {
 	}
 }
 
-export const Login = async (data: LoginData) => {
+export const login = async (data: LoginData) => {
 	const response = await fetch(
 		host + "/auth/get_session",
+		{
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			method: 'POST'
+		},
+	)
+	if (response.ok) {
+		return "ok";
+	}
+	else {
+		return (response.text())
+	}
+}
+
+export const fillAbout = async (data: FillAboutData) => {
+	const response = await fetch(
+		host + "/user/fill_about",
 		{
 			body: JSON.stringify(data),
 			headers: {
