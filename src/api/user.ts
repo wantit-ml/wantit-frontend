551a2e34c60f80909fbb5e8a50ab6a808109d367
fodro <feodor.tomilov@yandex.ru>
2021-06-25 17:44:37 +0300

    feat: implemented login

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,15 +1,20 @@
-export type NewUserProps = {
+export type NewUserData = {
 	username: string,
 	email: string,
 	phone: string,
 	role: string,
 }
 
+export type LoginData = {
+	username: string,
+	password: string,
+}
+
 let host = process.env["WANTIT_BACK"]
 if (!host)
 	host = "localhost/api"
 
-export const NewUser = async (data: NewUserProps) => {
+export const NewUser = async (data: NewUserData) => {
 	const response = await fetch(
 		host + "/auth/registration",
 		{
@@ -25,5 +30,24 @@ export const NewUser = async (data: NewUserProps) => {
 	}
 	else{
 		return(response.text())
+	}
+}
+
+export const Login = async (data: LoginData) => {
+	const response = await fetch(
+		host + "/auth/get_session",
+		{
+			body: JSON.stringify(data),
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			method: 'POST'
+		},
+	)
+	if (response.ok) {
+		return "ok";
+	}
+	else {
+		return (response.text())
 	}
 }
